f95071486a8c781e1162fd9f3a55ab9aeef0eccc
nicolascou <nicolascousillas1@gmail.com>
2023-05-11 14:41:28 +0200

    add about me

--- js/script.js
+++ js/script.js
@@ -6,9 +6,12 @@ hamburgerBtn.addEventListener('click', () => {
   closeBtn.classList.remove('header__menu-btn--hide');
   hamburgerBtn.classList.add('header__menu-btn--hide');
   mobileNav.classList.remove('header__navbar--hide');
+  mobileNav.classList.remove('animate__fadeOutRight');
+  mobileNav.classList.add('animate__fadeInLeft');
 });
 closeBtn.addEventListener('click', () => {
   hamburgerBtn.classList.remove('header__menu-btn--hide');
   closeBtn.classList.add('header__menu-btn--hide');
-  mobileNav.classList.add('header__navbar--hide');
+  mobileNav.classList.add('animate__fadeOutRight');
+  mobileNav.classList.remove('animate__fadeInLeft');
 });
